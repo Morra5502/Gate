5a73692fe28fc1288a33410a7c45b9af287b6960
Morra5502 <Danilshirnin999@gmail.com>
2024-12-20 10:47:15 +0700

    save esp

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ DB_CONFIG = {
     "port": "5433"
 }
 
-ESP_URL = "http://192.168.1.100"
+#ESP_URL = "http://192.168.1.100"
+ESP8266_IP = "192.168.0.18"  # Укажите IP ESP8266
+ESP8266_PORT = 8080  # Укажите порт ESP8266
 CAMERA_INDEX = 0
 RECOGNITION_DELAY = 5
